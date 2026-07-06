5f1448f2e3ce5217d8bbab4e3a55ace44e31ff31
lochana-team <matheeshpehesara321@gmail.com>
2026-07-06 17:32:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -100,13 +100,13 @@ async function connectToWA() {
 
       robin.sendMessage(ownerNumber + "@s.whatsapp.net", {
         image: {
-          url: `https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20cs.jpg`,
+          url: `https://raw.githubusercontent.com/lochana-team/LOCHANA-MD-HELPER/refs/heads/main/file_0000000014807207bf5f10d98373e997.png`,
         },
         caption: up,
       });
       robin.sendMessage("94729169740@s.whatsapp.net", {
         image: {
-          url: `https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20cs.jpg`,
+          url: `https://raw.githubusercontent.com/lochana-team/LOCHANA-MD-HELPER/refs/heads/main/file_0000000014807207bf5f10d98373e997.png`,
         },
         caption: up1,
       });
