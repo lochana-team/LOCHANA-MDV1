5818c64710f5df681d1655edce3eb00e14f95bfa
lochana-team <matheeshpehesara321@gmail.com>
2026-07-06 17:48:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
+  SESSION_ID: process.env.SESSION_ID || "c7BhXLJZ#tq-pCAyR9p7HYz1fYCPdEmwpEmjKNBMLEjgG6c21qBs",
   MONGODB: process.env.MONGODB || "Enter your mongoDB public URL",
-  OWNER_NUM: process.env.OWNER_NUM || "9470xxxxxxx",
+  OWNER_NUM: process.env.OWNER_NUM || "94775497858",
 };
